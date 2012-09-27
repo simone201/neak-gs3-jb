b4d64fb5f439bc65e6f961c2f4f739d6fc05fa6e
Simone Renzo <simone201@gmail.com>
2012-09-27 21:17:27 +0200

    midas-power: modded voltages limits

--- arch/arm/mach-exynos/midas-power.c
+++ arch/arm/mach-exynos/midas-power.c
@@ -547,7 +547,7 @@ static struct regulator_init_data max77686_buck2_data = {
 static struct regulator_init_data max77686_buck3_data = {
 	.constraints = {
 		.name = "vdd_int range",
-		.min_uV = 850000,
+		.min_uV = 600000,
 #ifdef CONFIG_SLP
 		.max_uV = 1150000,
 #else
@@ -566,9 +566,9 @@ static struct regulator_init_data max77686_buck4_data = {
 		.name = "vdd_g3d range",
 		.min_uV = 600000,
 #ifdef CONFIG_SLP
-		.max_uV = 1100000,
+		.max_uV = 1200000,
 #else
-		.max_uV = 1075000,
+		.max_uV = 1200000,
 #endif
 		.boot_on = 1,
 		.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE |
