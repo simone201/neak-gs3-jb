b6f630b12761b9adcc134259fdc2c5f09c4309f4
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-10-09 14:50:48 -0700

    mdnie: black crush fix. (Thanks to AndreiLux)

--- drivers/video/samsung/mdnie_table_c1m0.h
+++ drivers/video/samsung/mdnie_table_c1m0.h
@@ -15,7 +15,7 @@ static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -38,7 +38,7 @@ static const unsigned short tune_dynamic_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -73,7 +73,7 @@ static const unsigned short tune_dynamic_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -101,7 +101,7 @@ static const unsigned short tune_dynamic_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -132,7 +132,7 @@ static const unsigned short tune_movie_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -163,7 +163,7 @@ static const unsigned short tune_movie_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -198,7 +198,7 @@ static const unsigned short tune_movie_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -238,7 +238,7 @@ static const unsigned short tune_movie_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -261,7 +261,7 @@ static const unsigned short tune_standard_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -284,7 +284,7 @@ static const unsigned short tune_standard_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -319,7 +319,7 @@ static const unsigned short tune_standard_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -347,7 +347,7 @@ static const unsigned short tune_standard_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -382,7 +382,7 @@ static const unsigned short tune_natural_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -417,7 +417,7 @@ static const unsigned short tune_natural_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -452,7 +452,7 @@ static const unsigned short tune_natural_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -492,7 +492,7 @@ static const unsigned short tune_natural_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
